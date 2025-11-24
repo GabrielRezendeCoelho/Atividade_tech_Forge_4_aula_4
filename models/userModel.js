dd0006ec0a8d3496903d9f96d3399ec342f808bf
Gabriel Rezende Coelho <gabriel10_rezende@hotmail.com>
2025-11-24 10:41:27 -0300

    Revert "feat: implementar RBAC, middlewares, rotas modularizadas e delete seguro de arquivos; atualizar frontend"
    This reverts commit d58fa242e4e2f97c7d0b5045f1068264e92e5144.

--- models/userModel.js
+++ models/userModel.js
@@ -24,8 +24,6 @@ function addUser(user) {
     id,
     username: user.username,
     passwordHash: user.passwordHash,
-    // role padrão 'user' se não for informado
-    role: user.role || 'user',
   };
   if (user.email) stored.email = user.email;
   users.push(stored);
